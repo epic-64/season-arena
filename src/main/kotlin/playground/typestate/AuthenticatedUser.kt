a9d8294eb69ce579b1197343d166903d7b02fff9
William Raendchen <william.raendchen@gmail.com>
2025-09-07 14:06:09 +0200

    improve AuthenticatedUser.kt example

--- src/main/kotlin/playground/typestate/AuthenticatedUser.kt
+++ src/main/kotlin/playground/typestate/AuthenticatedUser.kt
@@ -4,28 +4,28 @@ sealed interface AuthState
 object Unauthenticated : AuthState
 object Authenticated : AuthState
 
-class User<S: AuthState>(val name: String, val passwordHash: String)
+data class User<S: AuthState>(val name: String, val passwordHash: String)
+data class AuthenticationException(override val message: String) : Exception(message)
+data class Session(val user: User<Authenticated>)
 
-fun authenticate(user: User<Unauthenticated>): User<Authenticated>? =
+fun authenticate(user: User<Unauthenticated>): Result<User<Authenticated>> =
     if (user.passwordHash == "hash")
-        User<Authenticated>(user.name, user.passwordHash)
+        Result.success(User<Authenticated>(user.name, user.passwordHash))
     else
-        null
+        Result.failure(AuthenticationException("Invalid password"))
 
-fun login(user: User<Authenticated>) {
-    println("Setting up session for ${user.name}")
-}
+fun login(user: User<Authenticated>): Session =
+    Session(user).also { println("Creating session for user: ${user.name}") }
 
 fun main() {
     val rawUser = User<Unauthenticated>("alice", "hashed_password")
-    val authedUser = authenticate(rawUser)
+    val authedResult = authenticate(rawUser)
 
-    if (authedUser == null) {
-        println("Authentication failed")
+    val authedUser = authedResult.getOrElse {
+        println("Authentication failed: ${it.message}")
         return
     }
 
-    login(authedUser)
-
-    // login(rawUser)
+    val session = login(authedUser)
+    // continue with all the exciting things a logged-in user can do
 }
