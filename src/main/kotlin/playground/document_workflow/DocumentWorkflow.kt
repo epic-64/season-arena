1f73d94464bdee87b615ace7986a60ef77d32e16
William Raendchen <william.raendchen@gmail.com>
2025-09-07 20:22:39 +0200

    cleanup

--- src/main/kotlin/playground/document_workflow/DocumentWorkflow.kt
+++ src/main/kotlin/playground/document_workflow/DocumentWorkflow.kt
@@ -6,33 +6,39 @@ object Reviewed : DocumentState
 object Published : DocumentState
 object Archived : DocumentState
 
-data class Document<S: DocumentState>(val id: String, val content: String)
+class Document<S: DocumentState> private constructor(val id: String, val content: String) {
+    companion object {
+        fun draft(id: String, content: String): Document<Draft> {
+            println("Creating draft document: $id")
+            return Document(id, content)
+        }
 
-fun reviewDocument(doc: Document<Draft>): Document<Reviewed> {
-    println("Reviewing document: ${doc.id}")
-    return Document(doc.id, doc.content)
-}
+        fun review(doc: Document<Draft>): Document<Reviewed> {
+            println("Reviewing document: ${doc.id}")
+            return Document(doc.id, doc.content)
+        }
 
-fun publishDocument(doc: Document<Reviewed>): Document<Published> {
-    println("Publishing document: ${doc.id}")
-    return Document(doc.id, doc.content)
-}
+        fun publish(doc: Document<Reviewed>): Document<Published> {
+            println("Publishing document: ${doc.id}")
+            return Document(doc.id, doc.content)
+        }
 
-fun archiveDocument(doc: Document<Published>): Document<Archived> {
-    println("Archiving document: ${doc.id}")
-    return Document(doc.id, doc.content)
-}
+        fun archive(doc: Document<Published>): Document<Archived> {
+            println("Archiving document: ${doc.id}")
+            return Document(doc.id, doc.content)
+        }
 
-fun <S: DocumentState> deleteDocument(doc: Document<S>): String {
-    println("Deleting document: ${doc.id}")
-    return "Document ${doc.id} deleted."
+        fun <S: DocumentState> delete(doc: Document<S>): String {
+            println("Deleting document: ${doc.id}")
+            return "Document ${doc.id} deleted."
+        }
+    }
 }
 
 fun main() {
-    val draft = Document<Draft>("doc-123", "This is a draft.")
-    val reviewed = reviewDocument(draft)
-    val published = publishDocument(reviewed)
-    val archived = archiveDocument(published)
-
-    val deleteMsg = deleteDocument(archived)
+    val draft = Document.draft("doc-123", "This is a draft.")
+    val reviewed = Document.review(draft)
+    val published = Document.publish(reviewed)
+    val archived = Document.archive(published)
+    val deletionMessage = Document.delete(archived)
 }
